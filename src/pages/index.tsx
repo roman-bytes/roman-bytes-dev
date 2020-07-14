77d6672f2219be65c3efa03198fdc751c2c68570
roman-ii <jcbrmn06@gmail.com>
2020-07-14 06:56:21 -0400

    inital commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FunctionComponent } from 'react';
+import { ReactElement } from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+
+const IndexPage: FunctionComponent = (): ReactElement => (
+    <Layout>
+        <SEO title="Home" />
+        
+    </Layout>
+);
+
+export default IndexPage;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                romanBlack: '#2B2A29'
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
+    purge: {
+        // Filenames to scan for classes
+        content: [
+            './src/**/*.html',
+            './src/**/*.js',
+            './src/**/*.jsx',
+            './src/**/*.ts',
+            './src/**/*.tsx',
+        ],
+        // PurgeCSS options
+        options: {
+            // Whitelist specific selectors by name
+            // whitelist: [],
+        },
+    },
+};
