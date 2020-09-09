eb4f67996e5c6840ca092373b76fb712e7c7f243
Jacob Roman <jroman@mindgruve.com>
2020-09-08 21:18:00 -0400

    adds in GAnalytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,12 @@ module.exports = {
                 path: `${__dirname}/src/images`,
             },
         },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: 'UA-9709289-17',
+            },
+        },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
