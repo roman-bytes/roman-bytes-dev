fd0a0d52859e9317dacd78d7ba2fe538e8d18ff7
Jacob Roman <jcbrmn06@gmail.com>
2021-03-15 12:27:30 -0400

    added faq function

---
+++ functions/faqs.js
@@ -0,0 +1,22 @@
+exports.handler = function(event, context, callback) {
+    const faqs = [{
+        question: 'Is this product waterproof?',
+        answer: 'Yes. You can take this anywhere and put it in any type of liquid.'
+    }, {
+        question: 'Is this product windproof?',
+        answer: 'Yes. You can withstand winds up to 200mph'
+    }, {
+        question: 'Will this product make you fly?',
+        answer: 'No. This is not Redbull and will not give you wings.'
+    }, {
+        question: 'Do they allow loud laughing in Hawaii?',
+        answer: 'No. Just a low ha.'
+    }]
+    callback(null, {
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        statusCode: 200,
+        body: faqs
+    });
+};
